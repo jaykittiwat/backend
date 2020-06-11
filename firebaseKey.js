f84cc4ded586d652fdbb38f1f2fb22108a1c420b
jaykittiwat <jaykittiwat2542@gmail.com>
2020-06-11 15:37:55 +0700

    Create firebaseKey.js

---
+++ firebaseKey.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqnYiZBBES0-5TrI2nSAWV64oxTqd-9JM",
+  authDomain: "testapp-ca98d.firebaseapp.com",
+  databaseURL: "https://testapp-ca98d.firebaseio.com",
+  projectId: "testapp-ca98d",
+  storageBucket: "testapp-ca98d.appspot.com",
+  messagingSenderId: "246118940753"
+  };
+
+ 
