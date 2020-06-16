0ec5fd8bbc897c90227de8f90ab20f2004e77833
jaykittiwat <jaykittiwat2542@gmail.com>
2020-06-17 02:06:37 +0700

    update to remote githup

--- userAPI/userAPI.js
+++ userAPI/userAPI.js
@@ -5,18 +5,29 @@ var firebase = require("../firebase.js");
 router.get("/logIn/:uid", (req, res) => {
   const UID = req.params.uid;
 
-  firebase.firebase().ref().child("user").orderByChild("email").equalTo(UID) .on("value", snapshot => {
+  firebase
+    .firebase()
+    .ref()
+    .child("User")
+    .orderByChild("email")
+    .equalTo(UID)
+    .on("value", snapshot => {
       var list = [];
       snapshot.forEach(elem => {
         list.push(elem.val());
       });
-      res.json(list)/*.status(201)*/;
+      res.json(list) /*.status(201)*/;
       //console.log(list);
     });
 });
-
+//บันทึกข้อมูลUser มีสมัครสมาชิก
 router.post("/registor", (req, res) => {
-  firebase.firebase().ref().child("user").push().set(req.body);
+  firebase
+    .firebase()
+    .ref()
+    .child("User")
+    .push()
+    .set(req.body);
   res.status(201);
 });
 
