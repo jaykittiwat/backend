268301e0a20ce630ecadf67ec0db7f90e130898b
jaykittiwat <jaykittiwat2542@gmail.com>
2020-08-29 23:42:12 +0700

    update to remote githup

--- routecow/setting/farm/drug.js
+++ routecow/setting/farm/drug.js
@@ -2,16 +2,26 @@ var express = require("express");
 var router = express.Router();
 var firebase = require("../../../firebase");
 //แบรนยาฟาร์ม
+router.get("/drug/:UID",(req,res)=>{
+  var uid = req.params.UID;
+
+  firebase
+    .firebase()
+    .ref("setting/farm/drug/" + uid)
+    .once("value", snapshot => {
+      var list = [];
+      var KEY = [];
+      var keyListgroup = [KEY, list];
+      snapshot.forEach(elem => {
+        KEY.push(elem.key); //วันที่ทั้งหมด
+        list.push(elem.val()); //รายการของแต่ละวันที่
+      });
+      res.json(keyListgroup);
+    });
+})
 router.post("/drug/:UID", (req, res) => {
   var uid = req.params.UID;
-  var data = {
-    common_drug: "บิวตาชิว",
-    dosage: "140",
-    drug_name: "บิวตาชิว",
-    exp_date: "2019-12-06",
-    mfd_date: "2009-05-06"
-  };
-
+  var data = req.body
   firebase
     .firebase()
     .ref("/setting/farm/drug/" + uid)
