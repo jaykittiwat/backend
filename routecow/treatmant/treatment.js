f0f0bbdf2cd88f4f5ababdcfd61fe53ee9b9f1f1
jaykittiwat <jaykittiwat2542@gmail.com>
2020-10-14 03:57:05 +0700

    update to remote githup

--- routecow/treatmant/treatment.js
+++ routecow/treatmant/treatment.js
@@ -8,12 +8,15 @@ router.post("/:UID", (req, res) => {
     
     res.status(201).json(req.body);
   });
-  router.post("/drug/:UID", (req, res) => {
-    const UID=req.params.UID;
-        firebase.firebase().ref("treatment/drug/"+UID).push(req.body)
-      
-      res.status(201).json(req.body);
-    });
+
+
+    router.post("/noti/:UID/:Date", (req, res) => {
+      const uid=req.params.UID;
+      var date = req.params.Date;
+      var data=req.body;
+          firebase.firebase().ref("notificationTreatment/" + uid + "/" + date).push(data)
+        res.status(201).json(req.body);
+      });
   
 
 module.exports = router;
