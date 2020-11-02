a96454841c3db7053f7d4c4247e4fa618ea66279
jaykittiwat <jaykittiwat2542@gmail.com>
2020-11-02 07:20:35 +0700

    update to remote githup

--- firebase.js
+++ firebase.js
@@ -1,13 +1,12 @@
 const firebase = require('firebase');
 var config = {
-  apiKey: "AIzaSyAr84xJjheE3ggfxEmdz_UzslZj2uOWKWM",
-  authDomain: "newtest-62eff.firebaseapp.com",
-  databaseURL: "https://newtest-62eff.firebaseio.com",
-  projectId: "newtest-62eff",
-  storageBucket: "newtest-62eff.appspot.com",
-  messagingSenderId: "581992453205",
-  appId: "1:581992453205:web:f487bad4f9341576b0f0dc",
-  measurementId: "G-G3BG5G49LF"
+  apiKey: "AIzaSyAqnYiZBBES0-5TrI2nSAWV64oxTqd-9JM",
+  authDomain: "testapp-ca98d.firebaseapp.com",
+  databaseURL: "https://testapp-ca98d.firebaseio.com",
+  projectId: "testapp-ca98d",
+  storageBucket: "testapp-ca98d.appspot.com",
+  messagingSenderId: "246118940753"
+
   };
   firebase.initializeApp(config);
 
@@ -18,3 +17,12 @@ var config = {
   exports.firebaseStorage=function () {
     return firebase.storage();
   }
+
+    /*apiKey: "AIzaSyAr84xJjheE3ggfxEmdz_UzslZj2uOWKWM",
+  authDomain: "newtest-62eff.firebaseapp.com",
+  databaseURL: "https://newtest-62eff.firebaseio.com",
+  projectId: "newtest-62eff",
+  storageBucket: "newtest-62eff.appspot.com",
+  messagingSenderId: "581992453205",
+  appId: "1:581992453205:web:f487bad4f9341576b0f0dc",
+  measurementId: "G-G3BG5G49LF"*/
