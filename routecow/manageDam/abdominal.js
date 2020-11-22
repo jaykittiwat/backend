f059fbe651a3708d26facbf27341c7f4423ce7a3
jaykittiwat <jaykittiwat2542@gmail.com>
2020-11-22 20:16:44 +0700

    update to remote githup

--- routecow/manageDam/abdominal.js
+++ routecow/manageDam/abdominal.js
@@ -45,6 +45,74 @@ router.get("/history/:uid/:cattle_id", (req, res) => {
       });
   });
 
+  router.get("/historyAllAbdominal/:uid/:startDate/:endDate", (req, res) => {
+    const startDate=req.params.startDate
+    const UID = req.params.uid;
+    const endDate=req.params.endDate
+    
+    firebase
+      .firebase()
+      .ref("abdominal/" + UID).orderByChild('dateabd').startAt(startDate).endAt(endDate)
+      .once("value", (snapshot) => {
+        const list = [];
+        const keylist = [];
+        snapshot.forEach((elem) => {
+          list.push(elem.val());
+          keylist.push(elem.key);
+        });
+  
+        res.json(list);
+      });
+  });
+  
+  router.get("/historyAllAbdominal/form01/:uid/:v/:mode", (req, res) => {
+  
+     const UID = req.params.uid;
+     const v=req.params.v
+     const mode=req.params.mode
+    firebase
+      .firebase()
+      .ref("abdominal/" + UID).orderByChild(mode).equalTo(v)
+      .once("value", (snapshot) => {
+        const list = [];
+        const keylist = [];
+        snapshot.forEach((elem) => {
+          list.push(elem.val());
+          keylist.push(elem.key);
+        });
+  
+        res.json(list);
+      });
+  });
+  
+  router.get("/historyAllAbdominal/form02/:uid/:v/:startDate/:endDate/:mode", (req, res) => {
+  
+    const startDate=req.params.startDate
+    const UID = req.params.uid;
+    const endDate=req.params.endDate
+    const v=req.params.v
+    const mode=req.params.mode
+   firebase
+     .firebase()
+     .ref("abdominal/" + UID).orderByChild('dateabd').startAt(startDate).endAt(endDate)
+     .once("value", (snapshot) => {
+       const list = [];
+       const keylist = [];
+       snapshot.forEach((elem) => {
+         const d =elem.val()
+         if(d[mode]===v){
+           list.push(elem.val());
+            keylist.push(elem.key);
+         }
+         
+       });
+  
+       res.json(list);
+     });
+  });
+  
+
+
     router.post("/:UID", (req, res) => {
     
         var uid = req.params.UID;
--- routecow/manageDam/breed.js
+++ routecow/manageDam/breed.js
@@ -103,6 +103,52 @@ router.get("/historyAllBreed/form02/:uid/:v/:startDate/:endDate/:mode", (req, re
 });
 
 
+router.get("/historyAllBreed/form03/:uid/:v/:mode", (req, res) => {
+
+  const UID = req.params.uid;
+  const v=req.params.v
+  const mode=req.params.mode
+ firebase
+   .firebase()
+   .ref("breed/" + UID).orderByChild(mode).equalTo(v)
+   .once("value", (snapshot) => {
+     const list = [];
+     const keylist = [];
+     snapshot.forEach((elem) => {
+       list.push(elem.val());
+       keylist.push(elem.key);
+     });
+
+     res.json(list);
+   });
+});
+
+router.get("/historyAllBreed/form04/:uid/:v/:startDate/:endDate/:mode", (req, res) => {
+
+ const startDate=req.params.startDate
+ const UID = req.params.uid;
+ const endDate=req.params.endDate
+ const v=req.params.v
+ const mode=req.params.mode
+firebase
+  .firebase()
+  .ref("breed/" + UID).orderByChild('date_breeding').startAt(startDate).endAt(endDate)
+  .once("value", (snapshot) => {
+    const list = [];
+    const keylist = [];
+    snapshot.forEach((elem) => {
+      const d =elem.val()
+      if(d[mode]===v){
+        list.push(elem.val());
+         keylist.push(elem.key);
+      }
+      
+    });
+
+    res.json(list);
+  });
+});
+
 ///////////////////////////////////////////////////////////
 
 router.get("/history/:uid/:cattle_id", (req, res) => {
