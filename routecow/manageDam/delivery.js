21ec4b1a645d447d0da4408489e6ca39a07233f5
jaykittiwat <jaykittiwat2542@gmail.com>
2020-11-23 21:21:08 +0700

    update to remote githup

--- routecow/manageDam/delivery.js
+++ routecow/manageDam/delivery.js
@@ -39,6 +39,74 @@ router.get("/history/:uid/:cattle_id", (req, res) => {
       });
   });
 
+  router.get("/historyAllDelivery/:uid/:startDate/:endDate", (req, res) => {
+    const startDate=req.params.startDate
+    const UID = req.params.uid;
+    const endDate=req.params.endDate
+    
+    firebase
+      .firebase()
+      .ref("delivery/" + UID).orderByChild('date').startAt(startDate).endAt(endDate)
+      .once("value", (snapshot) => {
+        const list = [];
+        const keylist = [];
+        snapshot.forEach((elem) => {
+          list.push(elem.val());
+          keylist.push(elem.key);
+        });
+  
+        res.json(list);
+      });
+  });
+  
+  router.get("/historyAllDelivery/form01/:uid/:v/:mode", (req, res) => {
+  
+     const UID = req.params.uid;
+     const v=req.params.v
+     const mode=req.params.mode
+    firebase
+      .firebase()
+      .ref("delivery/" + UID).orderByChild(mode).equalTo(v)
+      .once("value", (snapshot) => {
+        const list = [];
+        const keylist = [];
+        snapshot.forEach((elem) => {
+          list.push(elem.val());
+          keylist.push(elem.key);
+        });
+  
+        res.json(list);
+      });
+  });
+  
+  router.get("/historyAllDelivery/form02/:uid/:v/:startDate/:endDate/:mode", (req, res) => {
+  
+    const startDate=req.params.startDate
+    const UID = req.params.uid;
+    const endDate=req.params.endDate
+    const v=req.params.v
+    const mode=req.params.mode
+   firebase
+     .firebase()
+     .ref("delivery/" + UID).orderByChild('date').startAt(startDate).endAt(endDate)
+     .once("value", (snapshot) => {
+       const list = [];
+       const keylist = [];
+       snapshot.forEach((elem) => {
+         const d =elem.val()
+         if(d[mode]===v){
+           list.push(elem.val());
+            keylist.push(elem.key);
+         }
+         
+       });
+  
+       res.json(list);
+     });
+  });
+
+
+
 router.post("/:UID", (req, res) => {
     
     var uid = req.params.UID;
