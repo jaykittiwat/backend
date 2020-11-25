1ef42e5993f7719d608a5b3ff7ece990ac6167db
jaykittiwat <jaykittiwat2542@gmail.com>
2020-11-25 20:22:09 +0700

    react-create-app on Heroku

--- routecow/manageDam/abortion.js
+++ routecow/manageDam/abortion.js
@@ -24,6 +24,91 @@ router.get("/history/:uid/:cattle_id", (req, res) => {
   });
 
 
+  router.get("/historyAllAbortion/:uid", (req, res) => {
+    const UID = req.params.uid;
+    firebase
+      .firebase()
+      .ref("abortion/" + UID)
+      .once("value", (snapshot) => {
+        const list = [];
+        const keylist = [];
+        snapshot.forEach((elem) => {
+          list.push(elem.val());
+          keylist.push(elem.key);
+        });
+  
+        res.json(list);
+      });
+  });
+  
+  router.get("/historyAllAbortion/:uid/:startDate/:endDate", (req, res) => {
+    const startDate=req.params.startDate
+    const UID = req.params.uid;
+    const endDate=req.params.endDate
+    
+    firebase
+      .firebase()
+      .ref("abortion/" + UID).orderByChild('date').startAt(startDate).endAt(endDate)
+      .once("value", (snapshot) => {
+        const list = [];
+        const keylist = [];
+        snapshot.forEach((elem) => {
+          list.push(elem.val());
+          keylist.push(elem.key);
+        });
+  
+        res.json(list);
+      });
+  });
+  
+  router.get("/historyAllAbortion/form01/:uid/:v/:mode", (req, res) => {
+  
+     const UID = req.params.uid;
+     const v=req.params.v
+     const mode=req.params.mode
+    firebase
+      .firebase()
+      .ref("abortion/" + UID).orderByChild(mode).equalTo(v)
+      .once("value", (snapshot) => {
+        const list = [];
+        const keylist = [];
+        snapshot.forEach((elem) => {
+          list.push(elem.val());
+          keylist.push(elem.key);
+        });
+  
+        res.json(list);
+      });
+  });
+  
+  router.get("/historyAllAbortion/form02/:uid/:v/:startDate/:endDate/:mode", (req, res) => {
+  
+    const startDate=req.params.startDate
+    const UID = req.params.uid;
+    const endDate=req.params.endDate
+    const v=req.params.v
+    const mode=req.params.mode
+   firebase
+     .firebase()
+     .ref("abortion/" + UID).orderByChild('date').startAt(startDate).endAt(endDate)
+     .once("value", (snapshot) => {
+       const list = [];
+       const keylist = [];
+       snapshot.forEach((elem) => {
+         const d =elem.val()
+         if(d[mode]===v){
+           list.push(elem.val());
+            keylist.push(elem.key);
+         }
+         
+       });
+  
+       res.json(list);
+     });
+  });
+
+
+
     router.post("/:UID", (req, res) => {
     
         var uid = req.params.UID;
